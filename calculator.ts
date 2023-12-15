d78435fb00f3fc1a7bb912c9ff6386a344cb4cb4
shiekhansar62 <shiekhansar62@gmail.com>
2023-12-15 14:17:41 -0800

    Initial commit

---
+++ calculator.ts
@@ -0,0 +1,19 @@
+var prompt = require("prompt-sync")();
+let n1 = parseInt(prompt('please enter your first number'))
+let n2 = parseInt(prompt('please enter your second number'))
+let operator = prompt('please enter your operator (+, -, *, /): ')
+if(operator == '+'){
+    console.log(`the answer of ${n1} ${operator}  ${n2} = ${n1 + n2}`) 
+}
+else if(operator == '-'){
+    console.log(`the answer of ${n1} ${operator}  ${n2} = ${n1 - n2}`) 
+}
+else if(operator == '*'){
+    console.log(`the answer of ${n1} ${operator} ${n2} = ${n1 * n2}`) 
+}
+else if(operator == '/'){
+    console.log(`the answer of ${n1}  ${operator} ${n2} = ${n1 / n2}`) 
+}else {
+console.log("please select the correct operator");
+}
+export {}; 
